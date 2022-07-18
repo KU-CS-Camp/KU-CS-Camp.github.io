65a8eb9fb3887188afc4671529478baff418f26b
KU-CS-Camp <108029475+KU-CS-Camp@users.noreply.github.com>
2022-07-18 14:37:57 -0500

    Update text-emotion.py

--- text-emotion.py
+++ text-emotion.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
